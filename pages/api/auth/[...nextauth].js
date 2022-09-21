4db5c7ed166959c8af338e0adaef6640ac776da2
Huzaifa Ali <80360413+huzaifali835@users.noreply.github.com>
2022-09-22 01:50:30 +0500

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,8 +6,8 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.SECRET,
+      clientSecret: process.env.SECRET,
     }),
     // ...add more providers here
   ],
