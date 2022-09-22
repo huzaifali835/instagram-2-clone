25772fa6e97e269f6996d6f673d0c3e4124dda2b
Huzaifa Ali <80360413+huzaifali835@users.noreply.github.com>
2022-09-22 21:40:56 +0500

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,12 +14,15 @@ export default NextAuth({
     signIn: "/auth/signin",
   },
   callbacks: {
-    async session({session, token}){
+    async session({ session, token, user }) {
       session.user.username = session.user.name
-        .split(" ").join("").toLocaleLowerCase();
-        session.user.uid = token.sub;
-        return session;
-    }
-  }
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+
+      session.user.uid = token.sub;
+      return session;
+    },
+  },
   secret: process.env.NEXT_PUBLIC_SECRET
 });
